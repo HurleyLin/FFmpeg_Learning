fedfba8ae254c51fc6c75fa59adde9810805e033
linxl <linxl @ 1150118636@qq.com>
2018-12-22 15:40:41 +0800

    更新文件

--- Data_processing/PCM_Data_Processing.cpp
+++ Data_processing/PCM_Data_Processing.cpp
@@ -167,4 +167,112 @@ int sample_cpm16le_to_pcm8(char *url)
 	return 0;
 }
 
+/*
+ * @Description: 将PCM16LE双声道音频采样数据转换为WAVE格式音频数据
+ * @param: 	pcmpath:pcm文件
+ * 			wavepath:wave文件
+ * 			sample_rate:采样频率
+ * 			channels：通道数
+ * @return: int
+ * @Author: Hurley	2018-12-22
+ */
+int sample_pcm16le_to_wave(const char *pcmpath, int channels, int sample_rate, const char *wavepath)
+{
+	typedef struct WAVE_HEADER{
+		char fccID[4];
+		unsigned long dwSize;
+		char fccType[4];
+	}WAVE_HEADER;
+
+	typedef struct WAVE_FNT{
+		char fccID[4];
+		unsigned long 	dwSize;
+		unsigned short 	wFormatTag;
+		unsigned short 	wChannels;
+		unsigned long 	dwSamplePerSec;
+		unsigned long 	dwAvgBytesPerSec;
+		unsigned short 	wBlockAlign;
+		unsigned short 	uiBitsPerSample;
+	}WAVE_FMT;
+
+	typedef struct WAVE_DATA{
+		char fccID[4];
+		unsigned long dwSize;
+	}WAVE_DATA;
+
+	if (channels == 0 || sample_rate == 0)
+	{
+		channels = 2;
+		sample_rate = 44100;
+	}
+
+	int bits = 16;
+
+	WAVE_HEADER pcmHEADER;
+	WAVE_FMT 	pcmFMT;
+	WAVE_DATA 	pcmDATA;
+
+	unsigned short m_pcmData;
+	FILE *fp,*fpout;
+
+	fp = fopen(pcmpath, "rb+");
+	if (fp == NULL)
+	{
+		printf("open pcm file error\n");
+		return -1;
+	}
+
+	fpout = fopen(wavepath, "wb+");
+	if (fpout == NULL)
+	{
+		printf("open wav file error\n");
+		return -1;
+	}
+
+	memcpy(pcmHEADER.fccID,"RIFF",strlen("RIFF"));
+	memcpy(pcmHEADER.fccType,"WAVE",strlen("WAVE"));
+	fseek(fpout,sizeof(WAVE_HEADER),1);
+
+	pcmFMT.dwSamplePerSec = sample_rate;
+	pcmFMT.dwAvgBytesPerSec = pcmFMT.dwSamplePerSec*sizeof(m_pcmData);
+	pcmFMT.uiBitsPerSample = bits;
+
+	memcpy(pcmFMT.fccID,"fmt",strlen("fmt"));
+	pcmFMT.dwSize = 16;
+	pcmFMT.wBlockAlign = 2;
+	pcmFMT.wChannels = channels;
+	pcmFMT.wFormatTag = 1;
+
+	fwrite(&pcmFMT,sizeof(WAVE_FMT),1,fpout);
+
+	memcpy(pcmDATA.fccID,"data",strlen("data"));
+	pcmDATA.dwSize = 0;
+	fseek(fpout,sizeof(WAVE_DATA),SEEK_CUR);
+
+	fread(&m_pcmData,sizeof(unsigned short),1,fp);
+	while(!feof(fp))
+	{
+		pcmDATA.dwSize += 2;
+		fwrite(&m_pcmData,sizeof(unsigned short),1,fpout);
+		fread(&m_pcmData,sizeof(unsigned short),1,fp);
+	}
+
+	pcmHEADER.dwSize = 44 + pcmDATA.dwSize;
+
+	rewind(fpout);
+	fwrite(&pcmHEADER,sizeof(WAVE_HEADER),1,fpout);
+	fseek(fpout,sizeof(WAVE_FMT),SEEK_CUR);
+	fwrite(&pcmDATA,sizeof(WAVE_DATA),1,fpout);
+
+	fclose(fp);
+	fclose(fpout);
+
+	return 0;
+}
+
+
+
+
+
+
 
